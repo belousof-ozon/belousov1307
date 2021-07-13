953b5a91b4223b7fced9d7797ffe00da10d75a08
alebelousov <belousof@inbox.ru>
2021-07-13 20:45:04 +0300

    second

---
+++ lesson_1/DZ1_Vozrast.py
@@ -0,0 +1,8 @@
+#Расчет возраста
+
+god_rozh=input("В каком году вы родились? ")
+god_rasch=input("В каком году нужно посчитать возраст? ")
+
+vozrast=(int(god_rasch)-int(god_rozh))
+
+print("Ваш возраст в", god_rasch,  vozrast, "лет.")
